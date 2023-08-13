e7d1e1d32a164cb82b5d481d6fc6321398dcfaee
esau-morais <esaumorais7@gmail.com>
2023-08-13 12:07:26 -0300

    feat(remove-element): add 2nd solution

---
+++ src/solutions/remove-element.spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "bun:test";
+import { removeElement } from "./remove-element";
+
+describe("cases", () => {
+  test("case 1", () => {
+    const nums = [3, 2, 2, 3];
+    const val = 3;
+
+    const result = removeElement(nums, val);
+    expect(nums).toEqual([2, 2]);
+    expect(result).toEqual(2);
+  });
+
+  test("case 2", () => {
+    const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+    const val = 2;
+
+    const result = removeElement(nums, val);
+    expect(nums).toEqual([0, 1, 3, 0, 4]);
+    expect(result).toEqual(5);
+  });
+});
