ad97b57d208d6006cc2b3ef38d65bf31e0d72de3
esau-morais <esaumorais7@gmail.com>
2023-08-19 12:39:52 -0300

    chore(majority-element): update condition to decrease runtime

--- src/solutions/majority-element.ts
+++ src/solutions/majority-element.ts
@@ -4,11 +4,10 @@ export function majorityElement(nums: number[]): number {
   let candidate = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (count === 0) {
-      candidate = nums[i];
-    }
+    if (count === 0) candidate = nums[i];
 
-    count += candidate === nums[i] ? 1 : -1;
+    if (candidate === nums[i]) count++;
+    else count--;
   }
 
   return candidate;
