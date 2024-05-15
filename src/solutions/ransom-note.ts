2285b5f9c3ac69f7545a9de28ce9af4202f59b44
esau-morais <esaumorais7@gmail.com>
2024-05-15 16:35:03 -0300

    fix(ransom-note): use hashmap

--- src/solutions/ransom-note.ts
+++ src/solutions/ransom-note.ts
@@ -1,5 +1,22 @@
-// O(n*m)
+// O(n+m)
 export function canConstruct(ransomNote: string, magazine: string): boolean {
-  const needleIdx = magazine.indexOf(ransomNote);
-  return needleIdx !== -1;
+  const magazineMap = new Map<string, number>();
+
+  for (const char of magazine) {
+    if (magazineMap.has(char)) {
+      magazineMap.set(char, magazineMap.get(char)! + 1);
+    } else {
+      magazineMap.set(char, 1);
+    }
+  }
+
+  for (const char of ransomNote) {
+    if (magazineMap.get(char)) {
+      magazineMap.set(char, magazineMap.get(char)! - 1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 }
